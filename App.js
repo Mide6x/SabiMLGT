7e73f59ec7ed8374b3a9968faf823a6d1a0a50f3
Olumide <adewoleolumide05@gmail.com>
2024-05-18 20:36:08 +0100

    Added Splash Screen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import * as SplashScreen from "expo-splash-screen";
 import WelcomeScreen from "./components/WelcomeScreen";
 import ManualUploadScreen from "./components/ManualUploadScreen";
 import ScanUploadScreen from "./components/ScanUploadScreen";
@@ -10,6 +11,25 @@ import CustomHeader from "./components/CustomHeader"; // Import custom header co
 const Stack = createStackNavigator();
 
 const App = () => {
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        // Artificially delay for two seconds to simulate a slow loading experience.
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        await SplashScreen.hideAsync();
+      }
+    };
+
+    prepare();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
